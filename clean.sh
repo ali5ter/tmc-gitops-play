a9130dd8f72a64830889625615924d04d5d83141
Alister Lewis-Bowen <alister@lewis-bowen.org>
2022-10-21 17:08:54 -0400

    update

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+#set -x 
 tmc cluster namespace delete alb-gitops-dev --cluster-name alb-gitops-dev-one -m eks -p eks
 tmc workspace delete alb-gitops-kuard-dev
 tmc cluster delete alb-gitops-dev-one -m eks -p eks
