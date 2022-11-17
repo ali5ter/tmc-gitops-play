3fed2f7e17bd5a8459297aa4059214159b739ad8
Alister Lewis-Bowen <alister@lewis-bowen.org>
2022-11-16 19:14:38 -0500

    refactor apply

--- apply.sh
+++ apply.sh
@@ -1,223 +1,217 @@
 #!/usr/bin/env bash
 #set -x 
 
-dump_files () {
-    delete=0
-    if [[ -f ${1} ]]
-    then
-        echo "Processing ${1}"
-    else
-        prev_commit=$(git rev-parse @~)
-        git checkout ${prev_commit} ${1}
-        echo "Processing ${1}"
-        delete=1
-    fi
-}
-
 apply_state () {
-    delete=0
-    if [[ -f ${1} ]]
-    then
-        echo "Processing ${1}"
+    local file="$1"
+    local state="$2"
+    local delete=0
+    local kind version package name
+
+    if [[ -f $file ]]; then
+        echo "Processing $file"
     else
         prev_commit=$(git rev-parse @~)
-        git checkout ${prev_commit} ${1}
-        echo "Processing ${1}"
+        git checkout "${prev_commit}" "$file" 
+        echo "Processing $file"
         delete=1
     fi
 
-    kind=$(grep -m 1 "kind:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
-    version=$(grep -m 1 "version:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
-    package=$(grep -m 1 "package:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
-    name=$(grep -m 1 "name:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
-    echo "${kind}/${version}/${package}/${name}"
+    if [[ $state == "dump" ]]; then
+        exit 0
+    fi
 
-    if [[ ${kind} == "ClusterGroup" ]]
-    then
-        if [[ delete -eq 1 ]]
-        then
-            tmc clustergroup delete ${name}
+    parse_value_for() {
+        local key="$1"
+        grep -m 1 "${key}:" "$file" | cut -d":" -f2 | awk '{$1=$1;print}'
+    }
+
+    parse_mgmt_cluster_from_json_file() {
+        local json_file="$1"
+        jq '.fullName.managementClusterName' "$json_file" | sed 's/\"//g'
+    }
+
+    parse_provisioner_from_json_file() {
+        local json_file="$1"
+        jq '.fullName.managementClusterName' "$json_file" | sed 's/\"//g'
+    }
+
+    type_cluster_group() {
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting cluster group $name"
+            tmc clustergroup delete "$name"
         else
-            # op=$(tmc clustergroup get ${name})
-            # if [[ $? -eq 0 ]]
-            if tmc clustergroup get ${name} &> /dev/null
-            then
-                echo "Already exists. Updating."
-                tmc clustergroup update ${name} -f ${1}
+            if tmc clustergroup get "$name" &> /dev/null; then
+                echo "Updating cluster group $name"
+                tmc clustergroup update "$name" -f "$file"
             else
-                echo "Does not exist. Creating."
-                tmc clustergroup create -f ${1}
+                echo "Creating cluster group $name"
+                tmc clustergroup create -f "$file"
             fi
         fi
-    fi
+    }
 
-    if [[ ${kind} == "Cluster" ]]
-    then
-        if [[ delete -eq 1 ]]
-        then
-            tmc cluster delete ${name}
+    type_cluster() {
+        local mgmt_cluster provisioner version
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting cluster $name"
+            tmc cluster delete "$name"
         else
-            #op=$(tmc cluster get ${name} -o json)
-            #search by name if the cluster exists in the global list
-            tmc cluster list | grep ${name}
-            if [[ $? -eq 0 ]]
-            then
-                echo "Found cluster ${name}"
-                echo "get the m & p"
-                tmc cluster list --name ${name} -o json  |  jq -c '.clusters[0]' > cluster-info.json
-                mgmt=$(cat cluster-info.json | jq '.fullName.managementClusterName' | sed 's/\"//g')
-                echo "managementClusterName: ${mgmt}"
-                prov=$(cat cluster-info.json | jq '.fullName.provisionerName'| sed 's/\"//g')                
-                echo "provisionerName:${prov}"                
-                echo "Already exists. Updating."
-                version=$(cat cluster-info.json | jq '.meta.resourceVersion' | sed 's/\"//g' )
+            if tmc cluster list | grep "$name" &> /dev/null; then
+                echo "Found cluster $name"
+                tmc cluster list --name "$name" -o json  |  jq -c '.clusters[0]' > cluster-info.json
+                echo "Retrieve the management cluster and provisioner"
+                mgmt_cluster=$(parse_mgmt_cluster_from_json_file cluster-info.json)
+                echo "Management cluster: $mgmt_cluster"
+                provisioner=$(parse_provisioner_from_json_file cluster-info.json)
+                echo "Provisioner:$provisioner"
+                echo "Updating cluster $name"
+                version=$(jq '.meta.resourceVersion' cluster-info.json | sed 's/\"//g' )
                 #sed -e "s/meta:/meta:\\n  resourceVersion: $version/g" ${1} > tmpfile.yaml
-                ./cluster_patch_yaml.sh ${1} ${version} tmpfile.yaml
+                ./cluster_patch_yaml.sh "$file" "$version" tmpfile.yaml
                 cat tmpfile.yaml           
-                tmc cluster update ${name} -m ${mgmt} -p ${prov} -f tmpfile.yaml -v 9                
+                tmc cluster update "$name" -m "$mgmt_cluster" -p "$provisioner" -f tmpfile.yaml -v 9
                 rm tmpfile.yaml
                 rm cluster-info.json
             else
-                echo "Does not exist. Creating.${1}"
-                tmc cluster create -f ${1}
+                echo "Creating cluster $name"
+                tmc cluster create -f "$file"
             fi
         fi
-    fi
+    }
 
-    if [[ ${kind} == "Namespace" ]]
-    then
-        
-        if [[ delete -eq 1 ]]
-        then                        
-            tmc cluster namespace delete ${name} --cluster-name ${clusterName}      
+    type_namespace() {
+        local cluster_name mgmt_cluster provisioner
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting namespace $name"
+            tmc cluster namespace delete "$name" --cluster-name "$cluster_name" 
         else
-            clusterName=$(grep -m 1 "clusterName:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')            
+            clusterName=$(grep -m 1 "clusterName:" "$file" | cut -d":" -f2 | awk '{$1=$1;print}')            
             #TODO: manage return code
-            echo "Found cluster ${clusterName}"
+            echo "Found cluster $clusterName"
 
-            echo "get the m & p"
-            tmc cluster list --name ${clusterName} -o json  |  jq -c '.clusters[0]' > cluster-info.json
-            
-            mgmt=$(cat cluster-info.json | jq '.fullName.managementClusterName' | sed 's/\"//g')
-            echo "managementClusterName: ${mgmt}"
+            echo "Retrieve the management cluster and provisioner"
+            tmc cluster list --name "$clusterName" -o json  |  jq -c '.clusters[0]' > cluster-info.json
+            mgmt_cluster=$(jq '.fullName.managementClusterName' cluster-info.json | sed 's/\"//g')
+            echo "Management cluster: $mgmt_cluster"
+            provisioner=$(jq '.fullName.provisionerName' cluster-info.json | sed 's/\"//g')                
+            echo "Provisioner: $provisioner"
 
-            prov=$(cat cluster-info.json | jq '.fullName.provisionerName'| sed 's/\"//g')                
-            echo "provisionerName:${prov}"  
-
-            tmc cluster namespace get ${name} --cluster-name ${clusterName} -m ${mgmt} -p ${prov} -o json > namespace.json            
-            if [[ $? -eq 0 ]]
-            then
-                echo "Found namespace ${name}"                
-                echo "Already exists. Updating."
-                tmc cluster namespace update -f ${1} --cluster-name ${clusterName}                
+            if tmc cluster namespace get "$name" --cluster-name "$clusterName" -m "$mgmt_cluster" -p "$provisioner" -o json > namespace.json; then         
+                echo "Updating namespace $name"
+                tmc cluster namespace update -f "$file" --cluster-name "$clusterName"                
             else
-                echo "Does not exist. Creating."
-                tmc cluster namespace create -f ${1}
+                echo "Creating namespace $name"
+                tmc cluster namespace create -f "$file"
             fi
             rm namespace.json
             rm cluster-info.json
         fi
-    fi
+    }
 
-    if [[ ${kind} == "Workspace" ]]
-    then
-        if [[ delete -eq 1 ]]
-        then
-            tmc workspace delete ${name}
+    type_workspace() {
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting workgroup $name"
+            tmc workspace delete "$name"
         else
-            # op=$(tmc workspace get ${name})
-            # if [[ $? -eq 0 ]]
-            if tmc workspace get ${name} &> /dev/null
-            then
+            if tmc workspace get "$name" &> /dev/null; then
                 echo "Already exists. Updating."
-                tmc workspace update ${name} -f ${1}
+                tmc workspace update "$name" -f "$file"
             else
                 echo "Does not exist. Creating."
-                tmc workspace create -f ${1}
+                tmc workspace create -f "$file"
             fi
         fi
-    fi
+    }
 
-    if [[ ${kind} == "ImagePolicy" ]]
-    then
-        #type=$(grep "type:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
+    type_image_policy() {
+        local cluster_name mgmt_cluster provisioner type command parent_name
         type="image-policy"
-        echo "${type}"
-        if [[ ${package} == "vmware.tanzu.manage.v1alpha.workspace.policy" ]]
-        then
+        if [[ ${package} == "vmware.tanzu.manage.v1alpha.workspace.policy" ]]; then
             command="workspace"
-            parent_name=$(grep "workspaceName:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
+            parent_name=$(grep "workspaceName:" "$file" | cut -d":" -f2 | awk '{$1=$1;print}')
         fi
-        if [[ delete -eq 1 ]]
-        then
-            tmc ${command} ${type} delete ${name} --workspace-name ${parent_name}
+
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting image policy $name"
+            tmc "$command" "$type" delete "$name" --workspace-name "$parent_name" 
         else
-            # op=$(tmc ${command} ${type} get ${name} --workspace-name ${parent_name})
-            # if [[ $? -eq 0 ]]
-            if tmc ${command} ${type} get ${name} --workspace-name ${parent_name} &> /dev/null
-            then
-                echo "Already exists. Updating."
-                tmc ${command} ${type} update ${name} --workspace-name ${parent_name} -f ${1}
+            if tmc "$command" "$type" get "$name" --workspace-name "$parent_name" &> /dev/null; then
+                echo "Updating image policy $name"
+                tmc "$command" "$type" update "$name" --workspace-name "$parent_name" -f "$file"
             else
-                echo "Does not exist. Creating."
-                tmc ${command} ${type} create -f ${1}
+                echo "Deleting image policy $name"
+                tmc "$command" "$type" create -f "$file"
             fi
         fi
-    fi
+    }
 
-    if [[ ${kind} == "NetworkPolicy" ]]
-    then
-        #type=$(grep "type:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
+    type_network_policy() {
+        local cluster_name mgmt_cluster provisioner type command parent_name
         type="network-policy"
-        echo "Type ${type}"
-        if [[ ${package} == "vmware.tanzu.manage.v1alpha.workspace.policy" ]]
-        then
+        if [[ ${package} == "vmware.tanzu.manage.v1alpha.workspace.policy" ]]; then
             command="workspace"
-            parent_name=$(grep "workspaceName:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
+            parent_name=$(grep "workspaceName:" "$file" | cut -d":" -f2 | awk '{$1=$1;print}')
         fi
-        if [[ delete -eq 1 ]]
-        then
-            tmc ${command} ${type} delete ${name} --workspace-name ${parent_name}
+
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting network policy $name"
+            tmc "$command" "$type" delete "$name" --workspace-name "$parent_name" 
         else
-            # op=$(tmc ${command} ${type} get ${name} --workspace-name ${parent_name})
-            # if [[ $? -eq 0 ]]
-            if tmc ${command} ${type} get ${name} --workspace-name ${parent_name} &> /dev/null
-            then
-                echo "Already exists. Updating."
-                tmc ${command} ${type} update ${name} --workspace-name ${parent_name} -f ${1}
+            if tmc "$command" "$type" get "$name" --workspace-name "$parent_name" &> /dev/null; then
+                echo "Updating network policy $name"
+                tmc "$command" "$type" update "$name" --workspace-name "$parent_name" -f "$file"
             else
-                echo "Does not exist. Creating."
-                tmc ${command} ${type} create -f ${1}
+                echo "Deleting network policy $name"
+                tmc "$command" "$type" create -f "$file"
             fi
         fi
-    fi
+    }
 
-
-    if [[ ${kind} == "IAMPolicy" ]]
-    then
-        if [[ ${package} == "vmware.tanzu.manage.v1alpha.workspace.iampolicy" ]]
-        then
+    type_iam_policy() {
+        local cluster_name mgmt_cluster provisioner type command parent_name
+        if [[ ${package} == "vmware.tanzu.manage.v1alpha.workspace.policy" ]]; then
             command="workspace"
+            workspace_name=$(grep "workspaceName:" "$file" | cut -d":" -f2 | awk '{$1=$1;print}')
         fi
-        workspaceName=$(grep -m 1 "workspaceName:" ${1} | cut -d":" -f2 | awk '{$1=$1;print}')
-        if [[ delete -eq 1 ]]
-        then
-            tmc ${command} iam delete ${workspaceName}
+
+        if [[ delete -eq 1 ]]; then
+            echo "Deleting iam policy $name"
+            tmc "$command" iam delete "$workspace_name" 
         else
-            # op=$(tmc ${command} iam get-policy ${workspaceName})
-            # if [[ $? -eq 0 ]]
-            if tmc ${command} iam get-policy ${workspaceName} &> /dev/null
+            if tmc "$command" iam get-policy "$workspace_name" &> /dev/null
             then
-                echo "Already exists. Updating."
-                sed -n '/roleBindings/,$p' ${1} > tmpfile.yaml
-                tmc ${command} iam update-policy ${workspaceName} -f tmpfile.yaml -v 9
+                echo "Updating iam policy $name"
+                sed -n '/roleBindings/,$p' "$file" > tmpfile.yaml
+                tmc ${command} iam update-policy "$workspace_name" -f tmpfile.yaml -v 9
                 rm tmpfile.yaml
-	    fi
+	        fi
         fi
-    fi
+    }
+
+    kind=$(parse_value_for "kind")
+    version=$(parse_value_for "version")
+    package=$(parse_value_for "package")
+    name=$(parse_value_for "name")
+    echo "${kind}/${version}/${package}/${name}"
+
+    case $kind in
+        "ClusterGroup")     type_cluster_group;;
+        "Cluster")          type_cluster;;
+        "Namespace")        type_namespace;;
+        "Workspace")        type_workspace;;
+        "ImagePolicy")      type_image_policy;;
+        "NetworkPolicy")    type_network_policy;;
+        "IAMPolicy")        type_iam_policy;;
+        *)                  echo "Unknown kind: $kind";;
+    esac
 }
 
-while read line; do apply_state ${line}; done < <(git diff --name-only HEAD HEAD~1 | grep yaml)
+dump_files () {
+    local file="$1"
+    apply_state "$file" dump
+}
 
-#while read line; do dump_files ${line}; done < <(git diff --name-only HEAD HEAD~1 | grep yaml)
+while read -r line; do apply_state "$line"; done < <(git diff --name-only HEAD HEAD~1 | grep yaml)
+
+#while read -r line; do dump_files "$line"; done < <(git diff --name-only HEAD HEAD~1 | grep yaml)
 
